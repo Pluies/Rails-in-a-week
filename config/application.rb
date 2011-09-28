ed94e31e781e4114faf459d5eee154b3630e1c6f
Florent Delannoy <florent.delannoy@gmail.com>
2011-09-28 20:56:39 +0200

    Assets problems should be fixed

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,9 @@ module Sample
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Allow live compiling of assets
+    config.assets.compile = true
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
